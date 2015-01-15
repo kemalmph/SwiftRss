502ade4b6cce5b8a11c50a17f44de1c249dada43
Daniel Metzing <daniel_metzing@epam.com>
2015-01-15 16:33:21 +0100

    Pushing project file.

---
+++ SwiftRss/Extensions/BarButtonExtension.swift
@@ -0,0 +1,28 @@
+//
+//  BarButtonExtension.swift
+//  SwiftRss
+//
+//  Created by Daniel Metzing on 1/15/15.
+//  Copyright (c) 2015 Daniel Metzing. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIBarButtonItem {
+    
+    func addURLBarButtonItemForViewController(#viewController : UIViewController) -> UIBarButtonItem {
+    
+        var barButtonItem : UIBarButtonItem! = UIBarButtonItem()
+        
+        //barButtonItem.image =
+        
+        
+        
+        
+        return barButtonItem
+    }
+    
+
+}
+
---
+++ SwiftRss/ViewControllers/SRMainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SRMainViewController.swift
+//  SwiftRss
+//
+//  Created by Daniel Metzing on 1/15/15.
+//  Copyright (c) 2015 Daniel Metzing. All rights reserved.
+//
+
+import UIKit
+
+class SRMainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+      
+        super.viewDidLoad()
+     
+    }
+    
+    
+
+}
